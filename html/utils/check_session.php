e6ea28f47dabc2cc436c105b80a97eaf2fc5ff3e
Lou <luana.martelli@heig-vd.ch>
2017-12-23 21:37:28 +0100

    Hide cookies

--- html/utils/check_session.php
+++ html/utils/check_session.php
@@ -2,6 +2,11 @@
 
 session_start();
 
+/* Prevents attacks in order to steal the session ID */
+ini_set('session.cookie_httponly', 1);
+/* Session ID cannot be passed through URLs */
+ini_set('session.use_only_cookies', 1);
+
 if(!isset($_SESSION['user'])){
     header('Location:../views/login.php');
 }
