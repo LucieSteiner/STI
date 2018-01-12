3a3ec19ac36c48169891aaee4d00d21e0b58b13b
Lou <luana.martelli@heig-vd.ch>
2018-01-12 14:41:35 +0100

    Add scripts

--- conf/ssl/enable-https.sh
+++ conf/ssl/enable-https.sh
@@ -5,20 +5,17 @@
 
 # Reference : www.digitalocean.com/community/tutorials/how-to-create-an-ssl-certificate-on-apache-for-centos-7
 
-sudo systemctl enable httpd.service
 sudo yum install mod_ssl
 
 if [ ! -d "/etc/ssl/private" ]; then
   sudo mkdir /etc/ssl/private
 fi
 
-sudo cp ./apache-selfsigned.key /etc/ssl/private/apache-selfsigned.key
-sudo cp ./apache-selfsigned.crt /etc/ssl/certs/apache-selfsigned.crt
-sudo cp ./dhparam.pem /etc/ssl/certs/dhparam.pem
+sudo cp ./conf/ssl/apache-selfsigned.key /etc/ssl/private/apache-selfsigned.key
+sudo cp ./conf/ssl/apache-selfsigned.crt /etc/ssl/certs/apache-selfsigned.crt
+sudo cp ./conf/ssl/dhparam.pem /etc/ssl/certs/dhparam.pem
 
-sudo cp ./ssl.conf /etc/httpd/conf.d/ssl.conf
-sudo cp ./non-ssl.conf /etc/httpd/conf.d/non-ssl.conf
-
-sudo systemctl restart httpd.service
+sudo cp ./conf/ssl/ssl.conf /etc/httpd/conf.d/ssl.conf
+sudo cp ./conf/ssl/non-ssl.conf /etc/httpd/conf.d/non-ssl.conf
 
 
